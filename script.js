ec2b1b0c95720c8b7bb913c762764e5073139824
Bombica Sebastian <93088764+Flacarel@users.noreply.github.com>
2024-04-11 20:52:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function hideSidebar()
     sidebar.style.display = 'none';
 }
 
+let currentSlide = 0;
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+  
+    if ((currentSlide === totalSlides - 1 && direction === 1) || (currentSlide === 0 && direction === -1)) {
+      return;
+    }
+  
+    currentSlide += direction;
+    
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${currentSlide * 100}px)`; 
+  }
