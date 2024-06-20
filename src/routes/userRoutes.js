1f739637b8f425c9f2c0ce7f2809f869cc7e3dd2
magartrantit <nazacser@gmail.com>
2024-06-20 18:42:28 +0300

    delete user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,47 +1,32 @@
-// Importăm funcțiile pentru crearea și autentificarea utilizatorilor din controller-ul de utilizatori
 const { createUser, loginUser, uploadProfileImage, getAllFoods, getProductDetails } = require('../controllers/userController');
-// Importăm funcția middleware pentru autentificarea token-urilor
 const authenticateToken = require('../middleware/authMiddleware');
 const { refreshToken } = require('../controllers/userController');
-// Funcția pentru gestionarea rutelor de utilizatori
-const userRoutes = (req, res) => {
-    // Dacă metoda cererii este POST și URL-ul este '/api/users', apelăm funcția pentru crearea unui utilizator
+const db = require('../config/dbConfig');
+
+const userRoutes = async (req, res) => {
     if (req.method === 'POST' && req.url === '/api/users') {
         createUser(req, res);
-    } 
-    // Dacă metoda cererii este POST și URL-ul este '/api/login', apelăm funcția pentru autentificarea unui utilizator
-    else if (req.method === 'POST' && req.url === '/api/login') {
+    } else if (req.method === 'POST' && req.url === '/api/login') {
         loginUser(req, res);
-    } 
-    // Dacă metoda cererii este POST și URL-ul este '/api/uploadProfileImage', apelăm funcția pentru încărcarea imaginii de profil
-    else if (req.method === 'POST' && req.url === '/api/uploadProfileImage') {
+    } else if (req.method === 'POST' && req.url === '/api/uploadProfileImage') {
         uploadProfileImage(req, res);
-    }
-    // Dacă metoda cererii este GET și URL-ul este '/api/protected', apelăm funcția middleware pentru autentificarea token-ului
-    // și apoi trimitem un răspuns cu un mesaj și datele utilizatorului
-    else if (req.method === 'GET' && req.url === '/api/protected') {
+    } else if (req.method === 'GET' && req.url === '/api/protected') {
         authenticateToken(req, res, () => {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'Protected route accessed', user: req.user }));
         });
-    } 
-    // Dacă metoda cererii este GET și URL-ul este '/api/foods', apelăm funcția pentru obținerea tuturor alimentelor din baza de date
-    else if (req.method === 'GET' && req.url === '/api/foods') {
+    } else if (req.method === 'GET' && req.url === '/api/foods') {
         getAllFoods(req, res);
-    }
-    // Dacă metoda cererii este GET și URL-ul începe cu '/api/foods/', apelăm funcția pentru obținerea detaliilor unui produs
-    else if (req.method === 'GET' && req.url.startsWith('/api/foods/')) {
+    } else if (req.method === 'GET' && req.url.startsWith('/api/foods/')) {
         const productId = req.url.split('/').pop();
-        req.params = { id: productId }; // Adăugăm parametrii în obiectul req
+        req.params = { id: productId };
         getProductDetails(req, res);
-    }
-    else if (req.method === 'POST' && req.url === '/api/refreshToken') {
+    } else if (req.method === 'POST' && req.url === '/api/refreshToken') {
         refreshToken(req, res);
-    }
-    else if (req.method === 'POST' && req.url === '/api/products') {
-        authenticateToken(req, res, () => {
+    } else if (req.method === 'POST' && req.url === '/api/products') {
+        authenticateToken(req, res, async () => {
             const form = new formidable.IncomingForm();
-            form.parse(req, (err, fields) => {
+            form.parse(req, async (err, fields) => {
                 if (err) {
                     res.writeHead(500, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'Error parsing form data' }));
@@ -49,31 +34,74 @@ const userRoutes = (req, res) => {
                 }
 
                 const productData = {
+                    code: fields.code,
                     url: fields.image_url,
                     product_name: fields.product_name,
-                    // alte câmpuri...
+                    brands: fields.brands,
+                    categories_en: fields.categories_en,
+                    countries_en: fields.countries_en,
+                    ingredients_text: fields.ingredients_text,
+                    allergens: fields.allergens,
+                    additives_en: fields.additives_en,
+                    food_groups_en: fields.food_groups_en,
+                    main_category_en: fields.main_category_en,
+                    image_url: fields.image_url,
+                    image_ingredients_url: fields.image_ingredients_url,
+                    image_nutrition_url: fields.image_nutrition_url,
+                    "energy-kcal_100g": fields["energy-kcal_100g"],
+                    fat_100g: fields.fat_100g,
+                    "saturated-fat_100g": fields["saturated-fat_100g"],
+                    carbohydrates_100g: fields.carbohydrates_100g,
+                    sugars_100g: fields.sugars_100g,
+                    fiber_100g: fields.fiber_100g,
+                    proteins_100g: fields.proteins_100g,
+                    salt_100g: fields.salt_100g,
+                    sodium_100g: fields.sodium_100g,
                 };
 
                 const query = 'INSERT INTO products SET ?';
-                db.query(query, productData, (err) => {
-                    if (err) {
-                        res.writeHead(500, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ error: 'Error inserting product into database' }));
-                        return;
-                    }
-
+                try {
+                    await db.query(query, productData);
                     res.writeHead(200, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ message: 'Product added successfully' }));
-                });
+                } catch (error) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ error: 'Error inserting product into database' }));
+                }
             });
         });
-    }
-    // Dacă niciuna dintre condițiile de mai sus nu este îndeplinită, trimitem un răspuns cu statusul 404 (Not Found)
-    else {
+    }else if (req.method === 'DELETE' && req.url.startsWith('/api/users/')) {
+        const userId = req.url.split('/').pop();
+        const query = 'DELETE FROM users WHERE id = ?';
+        db.query(query, [userId], (err, results) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Error deleting user' }));
+                return;
+            }
+            if (results.affectedRows === 0) {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'User not found' }));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'User deleted successfully' }));
+        });
+    } 
+     else if (req.method === 'GET' && req.url === '/api/users') {
+        const query = 'SELECT id, username FROM users';
+        try {
+            const [results] = await db.query(query);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(results));
+        } catch (error) {
+            res.writeHead(500);
+            res.end(JSON.stringify({ error: 'Error fetching users' }));
+        }
+    } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 Not Found');
     }
 };
 
-// Exportăm funcția pentru gestionarea rutelor de utilizatori pentru a putea fi folosită în alte module
 module.exports = userRoutes;
